4e1f5b16bc7121f556c4ccab5d478c33250e4803
ayyappapalagani1 <palaganiayyappa@hotmail.com>
2025-05-19 18:52:41 +0530

    Add files via upload

---
+++ src/test/java/com/example/demo/SbrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
